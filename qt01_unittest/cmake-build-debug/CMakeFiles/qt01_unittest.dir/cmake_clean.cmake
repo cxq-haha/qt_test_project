87f6412d5917a913e401da5037c4565dd443a9bf
XiaoLiMa <1979553877@qq.com>
2021-08-07 16:56:09 +0800

    created project

---
+++ qt01_unittest/cmake-build-debug/CMakeFiles/qt01_unittest.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qt01_unittest.dir/main.cpp.obj"
+  "CMakeFiles/qt01_unittest.dir/qt01_unittest_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/qt01_unittest_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/qt01_unittest_autogen.dir/ParseCache.txt"
+  "qt01_unittest_autogen"
+  "libqt01_unittest.dll.a"
+  "qt01_unittest.exe"
+  "qt01_unittest.exe.manifest"
+  "qt01_unittest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/qt01_unittest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ qt01_unittest/cmake-build-debug/CMakeFiles/qt01_unittest_autogen.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for qt01_unittest_autogen.
+# This may be replaced when dependencies are built.
